65a77fa189f0e89949cdde68d6cc1cb4db57901a
zhangxin <zhangxin@canaan-creative.com>
2019-11-04 11:28:00 +0800

    update

--- hello_world/main.c
+++ hello_world/main.c
@@ -12,35 +12,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <bsp.h>
+#include <sysctl.h>
 
-#ifndef _BSP_SYSCALLS_H
-#define _BSP_SYSCALLS_H
-
-#include <machine/syscall.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void __attribute__((noreturn)) sys_exit(int code);
-
-void setStats(int enable);
-
-#undef putchar
-int putchar(int ch);
-void printstr(const char *s);
-
-void printhex(uint64_t x);
-
-size_t get_free_heap_size(void);
-
-#ifdef __cplusplus
+int core1_function(void *ctx)
+{
+    uint64_t core = current_coreid();
+    printf("Core %ld Hello world\n", core);
+    while(1);
 }
-#endif
 
-#endif /* _BSP_SYSCALLS_H */
+int main(void)
+{
+    sysctl_pll_set_freq(SYSCTL_PLL0, 800000000);
+    uint64_t core = current_coreid();
+    int data;
+    printf("Core %ld Hello world\n", core);
+    register_core1(core1_function, NULL);
 
+    /* Clear stdin buffer before scanf */
+    sys_stdin_flush();
+
+    scanf("%d", &data);
+    printf("\nData is %d\n", data);
+    while(1)
+        continue;
+    return 0;
+}
